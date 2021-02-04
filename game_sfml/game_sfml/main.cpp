51f335ef833f2815d9bf23a5a7b710300ad1ac04
EgorNered <enered300@gmail.com>
2021-02-04 18:32:36 +0300

    Добавил передвижение и почти сделал анимацию

--- game_sfml/game_sfml/main.cpp
+++ game_sfml/game_sfml/main.cpp
@@ -6,10 +6,10 @@ using namespace sf;
 int main()
 {
 	RenderWindow window(VideoMode(1920, 1080), "game");
-	window.setFramerateLimit(60); // количество кадров в секунду
+	window.setFramerateLimit(60); // количество кадров в секунду (60)
     //персонаж
 	Image heroimage;
-	heroimage.loadFromFile("images/pers.jpg");
+	heroimage.loadFromFile("images/pers.png");
 
 
 	Texture herotexture;
@@ -17,10 +17,12 @@ int main()
 
 	Sprite herosprite;
 	herosprite.setTexture(herotexture);
-	herosprite.setTextureRect(IntRect(0, 0, 362, 364));
+	herosprite.setTextureRect(IntRect(4,775,101,774));
 	herosprite.setPosition(10, 5);
 	//
 	
+	
+	float CurrentFrame = 0; //Хранит текущий кадр
 	//Игровое время
 	Clock clock;
 
@@ -37,10 +39,30 @@ int main()
 				window.close();
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::A)) { herosprite.move(-0.1 * time, 0); }
-		if (Keyboard::isKeyPressed(Keyboard::D)) { herosprite.move(0.1 * time, 0); }
-		if (Keyboard::isKeyPressed(Keyboard::W)) { herosprite.move(0, -0.1 * time); }
-		if (Keyboard::isKeyPressed(Keyboard::S)) { herosprite.move(0, 0.1 * time); }
+		if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) { //если нажата клавиша стрелка влево или англ буква А
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости
+			if (CurrentFrame > 9) CurrentFrame -= 9; // если пришли к 9 кадру - откидываемся назад.
+			herosprite.setTextureRect(IntRect(100 * int(CurrentFrame), 559, 1020, 100)); //проходимся по координатам Х.
+			herosprite.move(-0.2 * time, 0);//происходит само движение персонажа влево
+		}
+		if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) { //если нажата клавиша стрелка влево или англ буква D
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости.
+			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+			herosprite.setTextureRect(IntRect(100 * int(CurrentFrame), #, #, #)); //проходимся по координатам Х.
+			herosprite.move(-0.1 * time, 0);//происходит само движение персонажа влево
+		}
+		if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) { //если нажата клавиша стрелка влево или англ буква W
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. 
+			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+			herosprite.setTextureRect(IntRect(100 * int(CurrentFrame), #, #, #)); //проходимся по координатам Х. 
+			herosprite.move(-0.1 * time, 0);//происходит само движение персонажа влево
+		}
+		if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) { //если нажата клавиша стрелка влево или англ буква S
+			CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. 
+			if (CurrentFrame > 3) CurrentFrame -= 3; // если пришли к третьему кадру - откидываемся назад.
+			herosprite.setTextureRect(IntRect(100 * int(CurrentFrame), #, #, #)); //проходимся по координатам Х. 
+			herosprite.move(-0.1 * time, 0);//происходит само движение персонажа влево
+		}
 
 		window.clear();
 		window.draw(herosprite);
